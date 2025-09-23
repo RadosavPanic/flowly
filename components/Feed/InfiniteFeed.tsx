fb63e65ae43c8911efcafb33bc7c4af70ff6e25b
RadosavPanic <radosav.panic@gmail.com>
2025-09-23 11:38:37 +0200

    updated endpoint for infinite feed

--- components/Feed/InfiniteFeed.tsx
+++ components/Feed/InfiniteFeed.tsx
@@ -6,7 +6,7 @@ import Post from "../Post/Post";
 
 const fetchPosts = async (pageParam: number, userProfileId?: string) => {
   const res = await fetch(
-    `http://localhost:3000/api/posts?cursor=${pageParam}&user=${userProfileId}`
+    `${process.env.APP_URL}/api/posts?cursor=${pageParam}&user=${userProfileId}`
   );
   return res.json();
 };
